9ba7b89447d181008c31f8315369fcf439985156
JayJ017 <mickypoo49@gmail.com>
2024-06-11 17:06:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,7 +63,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!\n\n» ᴍʏ ᴏᴡɴᴇʀ : @codeflix_bots"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1231247732)
+ADMINS.append(6497757690)
 
 LOG_FILE_NAME = "codeflixbot.txt"
 
